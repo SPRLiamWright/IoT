de5225c0196e4269f06a79ecc0406a7f75be775d
SPRLiamWright <s3550247@student.rmit.edu.au>
2020-03-16 17:14:39 +1100

    Added comments to SenseHatHello

--- Self Curiosity/SenseHatHello.py
+++ Self Curiosity/SenseHatHello.py
@@ -1,8 +1,12 @@
+#Display simple scrolling text using SenseHat
+
 from sense_hat import SenseHat
 
 sense = SenseHat()
 
+#Foreground will be bright, soft pink
 fground = (255,128,255)
+#Background will be dim, dark green
 bground = (0,64,0)
 
 sense.show_message("Hello World!", text_colour = fground, back_colour = bground)
